3afb850a1d7fe315ac68366f3d4cca4d253315c7
zhanetfio <97479685+zhanetfio@users.noreply.github.com>
2022-01-24 20:02:15 +0300

    Add files via upload

---
+++ use-bracket-notation-to-find-the-nth-to-last-character-in-a-string.js
@@ -0,0 +1,6 @@
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line. 
+//Use bracket notation to find the second-to-last character in the lastName string
+const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
