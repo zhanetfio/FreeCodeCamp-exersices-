0e7ba49bddd854a7e37bf337289bdb577b36a347
zhanetfio <97479685+zhanetfio@users.noreply.github.com>
2022-01-24 20:11:50 +0300

    Create word-blanks.js

---
+++ word-blanks.js
@@ -0,0 +1,10 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+/*You will also need to account for spaces in your string, so that the final sentence has spaces
+ between all the words. The result should be a complete sentence.*/
+const wordBlanks = "The "+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb; // Change this line
+// Only change code above this line
